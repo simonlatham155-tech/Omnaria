98437d053a52fabe32c107cf430f95a7fba426d1
simonlatham155-tech <simonlatham155@gmail.com>
2026-08-15 10:18:36 +0200

    Use neutral Omnaria engine state in globe

--- Source/ui/WorldGlobe.h
+++ Source/ui/WorldGlobe.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <JuceHeader.h>
-#include "../dsp/LatWorldState.h"
+#include "../dsp/OmnariaState.h"
 
 namespace omnaria
 {
@@ -9,7 +9,7 @@ class WorldGlobe final : public juce::Component,
                          private juce::Timer
 {
 public:
-    explicit WorldGlobe(const LatWorldState& worldState);
+    explicit WorldGlobe(const OmnariaState& engineState);
     ~WorldGlobe() override = default;
 
     void paint(juce::Graphics& g) override;
@@ -18,7 +18,7 @@ private:
     void timerCallback() override;
     juce::Point<float> projectParticle(float longitude, float latitude, float radius, juce::Point<float> centre) const;
 
-    const LatWorldState& state;
+    const OmnariaState& state;
     float animationPhase { 0.0f };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WorldGlobe)
