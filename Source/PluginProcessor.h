015561b3e45d7eddfbbc305b40dafbb247cbc4f3
simonlatham155-tech <simonlatham155@gmail.com>
2026-08-17 22:07:57 +0200

    Add Engine 3 plugin processor

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <JuceHeader.h>
+#include "Engine3Voice.h"
+
+class OmnariaEngine3TestAudioProcessor : public juce::AudioProcessor
+{
+public:
+    OmnariaEngine3TestAudioProcessor();
+    ~OmnariaEngine3TestAudioProcessor() override = default;
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override {}
+    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+
+    const juce::String getName() const override { return JucePlugin_Name; }
+    bool acceptsMidi() const override { return true; }
+    bool producesMidi() const override { return false; }
+    bool isMidiEffect() const override { return false; }
+    double getTailLengthSeconds() const override { return 4.0; }
+
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram(int) override {}
+    const juce::String getProgramName(int) override { return {}; }
+    void changeProgramName(int, const juce::String&) override {}
+
+    void getStateInformation(juce::MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
+
+    juce::AudioProcessorValueTreeState apvts;
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+private:
+    juce::Synthesiser synth;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(OmnariaEngine3TestAudioProcessor)
+};
